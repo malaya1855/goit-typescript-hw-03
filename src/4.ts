7d372ece3d8cc14ef5df8760441b910ad8bed5ae
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-12-15 11:32:20 +0200

    solved tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,48 @@
+class Key {
+     signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  class Person {
+     key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+
+  abstract class House {
+     door: boolean;
+     key: Key;
+     tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.door = false; 
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void; 
+    comeIn(person: Person): void {
+        this.door && this.tenants.push(person)
+    }
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+       (key.getSignature() === this.key.getSignature()) ? this.door = true : this.door = false
+    }
+  }
+  
 const key = new Key();
 
 const house = new MyHouse(key);
